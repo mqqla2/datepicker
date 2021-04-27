98dc1db99ecbe1d6d8d1395cb2edf70faf619f36
mkukula <mkukula@gmail.com>
2021-04-27 16:35:33 +0200

    update

--- datepicker.js
+++ datepicker.js
@@ -7,9 +7,8 @@
  `;
  class DatePicker extends HTMLElement {
   constructor() {
-  alert('dupa!!!')
       super();
-      this.appendChild(datepicker.content.cloneNode(true));
+      this.appendChild(datepicker.cloneNode(true));
       this.addEventListener("click", event => {
       var event = new Event("onClick");
       this.dispatchEvent(event);
@@ -20,6 +19,7 @@
         this._props = { ...this._props, ...changedProperties };
       }
       onCustomWidgetAfterUpdate(changedProperties) {
+
       }
   }
   customElements.define("com-sap-sample-reginek-datepicker", DatePicker);
